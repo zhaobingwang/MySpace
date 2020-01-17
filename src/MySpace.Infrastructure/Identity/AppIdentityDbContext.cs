8a553cb6e20764cbea6a86936a56ef463f30797a
zhaobingwang <zhaobingwang@foxmail.com>
2020-01-17 13:33:28 +0800

    feat: 自定义Identity表名 & 添加数据迁移，初始化数据库

--- src/MySpace.Infrastructure/Identity/AppIdentityDbContext.cs
+++ src/MySpace.Infrastructure/Identity/AppIdentityDbContext.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace MySpace.Infrastructure.Identity
 {
@@ -13,6 +15,41 @@ namespace MySpace.Infrastructure.Identity
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>(b =>
+            {
+                b.ToTable("Users");
+            });
+
+            builder.Entity<IdentityUserClaim<string>>(b =>
+            {
+                b.ToTable("UserClaims");
+            });
+
+            builder.Entity<IdentityRole>(b =>
+            {
+                b.ToTable("Roles");
+            });
+
+            builder.Entity<IdentityRoleClaim<string>>(b =>
+            {
+                b.ToTable("RoleClaims");
+            });
+
+            builder.Entity<IdentityUserRole<string>>(b =>
+            {
+                b.ToTable("UserRoles");
+            });
+
+            builder.Entity<IdentityUserToken<string>>(b =>
+            {
+                b.ToTable("UserTokens");
+            });
+
+            builder.Entity<IdentityUserLogin<string>>(b =>
+            {
+                b.ToTable("UserLogins");
+            });
         }
     }
 }
