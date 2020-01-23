caf84aab74a8c53b7e78e78633209253414bbcab
zhaobingwang <zhaobingwang@foxmail.com>
2020-01-23 14:02:49 +0800

    chore: 添加utilities docfx cmd

---
+++ docfx.utilities.cmd
@@ -0,0 +1,15 @@
+set docfxPath=..\..\tools\docfx\docfx.exe
+
+mkdir .docfx\MySpace.Utilities
+cd .docfx\MySpace.Utilities
+
+%docfxPath% init -q
+%docfxPath% .\docfx_project\docfx.json
+
+docfx metadata ..\..\src\MySpace.Utilities\MySpace.Utilities.csproj
+
+xcopy /d /y "_api\*" "docfx_project\api\"
+
+%docfxPath% .\docfx_project\docfx.json
+%docfxPath% serve .\docfx_project\_site\
+pause
