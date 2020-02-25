b97a5d783510e0f7f8f207e4a49909c9b15e1f97
zhaobingwang <zhaobingwang@foxmail.com>
2020-02-25 21:26:33 +0800

    feat(MySpace.Utilities): 将对象序列化为Json #1

--- src/MySpace.Utilities/Json/JsonUtil.cs
+++ src/MySpace.Utilities/Json/JsonUtil.cs
@@ -2,14 +2,37 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Text.Json;
+using MySpace.Utilities.Extensions;
 
 namespace MySpace.Utilities
 {
     public static partial class JsonUtil
     {
+        /// <summary>
+        /// 将Json字符串转换为对象
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="json">Json字符串</param>
+        /// <returns></returns>
         public static T ToObject<T>(string json)
         {
+            if (json.IsNullOrWhiteSpace())
+                return default;
+
             return JsonSerializer.Deserialize<T>(json);
+        }
+
+        /// <summary>
+        /// 将对象转换为Json字符串
+        /// </summary>
+        /// <param name="target">目标对象</param>
+        /// <returns></returns>
+        public static string ToJson(object target)
+        {
+            if (target == null)
+                return string.Empty;
+            var result = JsonSerializer.Serialize(target);
+            return result;
         }
     }
 }
--- tests/MySpace.Utilities.UnitTests/Json/JsonUtilTest.cs
+++ tests/MySpace.Utilities.UnitTests/Json/JsonUtilTest.cs
@@ -8,16 +8,47 @@ namespace MySpace.Utilities.UnitTests
     [Trait("工具类", "Json")]
     public class JsonUtilTest
     {
-        [Fact(DisplayName = "Json转对象-成功测试")]
+        [Fact(DisplayName = "Json转对象-正常入参")]
         public void ToObject_ShouldSuccess_WithExpectedParameters()
         {
-            var json = "{\"Location\":\"Hangzhou\",\"Temperature\":4,\"Date\":\"2019-11-19T11:55:46.0310797+00:00\"}";
+            var json = "{\"Location\":\"Hangzhou\",\"Temperature\":4,\"Date\":\"2020-02-25T21:03:37+08:00\"}";
             var result = JsonUtil.ToObject<WeatherForecast>(json);
 
             Assert.True(result.Location == "Hangzhou");
             Assert.True(result.Temperature == 4);
             Assert.IsType<DateTimeOffset>(result.Date);
+            Assert.True(result.Date == new DateTimeOffset(2020, 2, 25, 21, 03, 37, TimeSpan.FromHours(8)));
         }
+
+        [Fact(DisplayName = "Json转对象-传入空值")]
+        public void ToObject_ReturnDefault_WithNull()
+        {
+            string json = "";
+            var result = JsonUtil.ToObject<WeatherForecast>(json);
+
+            Assert.True(result == default(WeatherForecast));
+        }
+
+        [Fact(DisplayName = "对象转Json-正常入参")]
+        public void ToJson_ShouldSuccess_WithExpectedParameters()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Location = "Hangzhou",
+                Temperature = 4,
+                Date = new DateTimeOffset(2020, 2, 25, 21, 03, 37, TimeSpan.FromHours(8))
+            };
+            var expected = new StringBuilder();
+            expected.Append("{");
+            expected.Append("\"Location\":\"Hangzhou\",");
+            expected.Append("\"Temperature\":4,");
+            expected.Append("\"Date\":\"2020-02-25T21:03:37+08:00\"");
+            expected.Append("}");
+
+            var result = JsonUtil.ToJson(weatherForecast);
+            Assert.Equal(expected.ToString(), result);
+        }
+
         class WeatherForecast
         {
             public string Location { get; set; }
