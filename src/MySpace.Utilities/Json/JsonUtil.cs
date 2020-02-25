a72a2998c1454bb043248c5285de3090fa120de1
zhaobingwang <zhaobingwang@foxmail.com>
2020-02-25 19:56:32 +0800

    feat(MySpace.Utilities): json util #1

---
+++ src/MySpace.Utilities/Json/JsonUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace MySpace.Utilities
+{
+    public static partial class JsonUtil
+    {
+        public static T ToObject<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
---
+++ tests/MySpace.Utilities.UnitTests/Json/JsonUtilTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MySpace.Utilities.UnitTests
+{
+    [Trait("工具类", "Json")]
+    public class JsonUtilTest
+    {
+        [Fact(DisplayName = "Json转对象-成功测试")]
+        public void ToObject_ShouldSuccess_WithExpectedParameters()
+        {
+            var json = "{\"Location\":\"Hangzhou\",\"Temperature\":4,\"Date\":\"2019-11-19T11:55:46.0310797+00:00\"}";
+            var result = JsonUtil.ToObject<WeatherForecast>(json);
+
+            Assert.True(result.Location == "Hangzhou");
+            Assert.True(result.Temperature == 4);
+            Assert.IsType<DateTimeOffset>(result.Date);
+        }
+        class WeatherForecast
+        {
+            public string Location { get; set; }
+            public int Temperature { get; set; }
+            public DateTimeOffset Date { get; set; }
+        }
+
+    }
+}
